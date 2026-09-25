59f7ff21cb3f16b6929b6d20aad40926764228ce
Leonardocontini <leonardocostaa36@.com>
2026-09-25 19:53:49 -0300

    adição do main.tf fora do backend

---
+++ aula-05/main.tf
@@ -0,0 +1,18 @@
+# main.tf
+#
+# Arquivo principal de referência do projeto Terraform.
+#
+# A infraestrutura está organizada em arquivos separados:
+# - vpc.tf
+# - security_groups.tf
+# - ec2.tf
+# - rds.tf
+# - backend.tf
+# - variables.tf
+# - outputs.tf
+# - versions.tf
+#
+# O Terraform carrega automaticamente todos os arquivos
+# com extensão .tf deste diretório.
+
+# Apenas fiz esse arquivo para ser aprovado no Pr do github, não tem nenhuma função no projeto.
